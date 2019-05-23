2da4b3d9ffe831eb3432c17f04e9b721e208901f
Skye <skye@domos.me.uk>
2019-05-23 19:13:15 +0100

    Registry working

--- Registry.php
+++ Registry.php
@@ -14,14 +14,27 @@ class Registry {
      * @author Skye
      */
     public function __call($name, $arguments){
-        echo "Called registry method $name<br>";
-        if (\strncmp($name,'get',3) === 0){
-            $key = \str_tokenize(\substr($name,3));
-            echo "Checking for array key $key";
-            if (\array_key_exists($key,$this->data)){
-                return $this->data[$key];
-            }
-            return null;
+        $method = \substr($name,0,3);
+        $key = \str_tokenize(\substr($name,3));
+        switch($method){
+            case 'has':
+                if (\array_key_exists($key,$this->data)){
+                    return true;
+                }
+                return false;
+            case 'get':
+                if (\array_key_exists($key,$this->data)){
+                    return $this->data[$key];
+                }
+                return null;
+            case 'set':
+                if(\count($arguments) === 1){
+                    $this->data[$key] = $arguments[0];
+                } else {
+                    throw new \ArgumentCountError("Exactly one argument expected to set registry value");
+                }
+            default:
+                throw new \Exception("Registry does not support the method $name");
         }
     }
 }
