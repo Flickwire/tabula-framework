29951c94b56cb49becc286b8a5667773a5a8538b
Skye <skye@domos.me.uk>
2019-05-26 22:54:01 +0100

    Database initialised

--- Tabula.php
+++ Tabula.php
@@ -10,11 +10,24 @@ class Tabula {
     public $registry;
     public $router;
     public $config;
+    public $db;
 
     public function __construct() {
         $this->registry = new Registry($this);
         $this->router = new Router($this);
         $this->config = new Config(\file_get_contents($this->registry->getFsBase() . 'config.json'));
+
+        if(!isset($this->config->database)){
+            throw new \Exception("No database details found in config");
+        }
+        $dbconf = $this->config->database;
+        switch ($dbconf['type']){
+            case "mysql":
+                $this->db = new Database\Adapter\MySqlAdapter($dbconf['host'], $dbconf['database'], $dbconf['user'], $dbconf['password'], isset($dbconf['port']) ? $dbconf['port'] : null, isset($dbconf['charset']) ? $dbconf['charset'] : null);
+                break;
+            default:
+                throw new \Exception("Unsupported database type {$dbconf['type']}");
+        }
 
         //Create the request object
         $this->registry->setRequest(new Request($this));
