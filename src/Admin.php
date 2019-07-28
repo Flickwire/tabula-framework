9619c6cdf88704a572181ef14560a282b3d94c65
Skye <skye@domos.me.uk>
2019-07-28 18:09:01 +0100

    Merge admin module into core

---
+++ src/Admin.php
@@ -0,0 +1,105 @@
+<?php 
+namespace Tabula;
+
+use Tabula\Router\Route;
+use Tabula\Database\Adapter\AbstractAdapter;
+use Tabula\Renderer\Page;
+
+class Admin {
+    
+    private $panes = [];
+    private $groups = [];
+    private $tabula;
+    private $activePane;
+
+    public function __construct(Tabula $tabula){
+        $this->tabula = $tabula;
+    }
+
+    public function init(): void{
+        if(class_exists("\Tabula\Modules\Auth\SecureRoute")){
+            $this->tabula->router->register(new \Tabula\Modules\Auth\SecureRoute($this->tabula,"/admin",$this,"render"));
+        } else {
+            $this->tabula->router->register(new Route("/admin",$this,"render"));
+        }
+    }
+
+    public function registerPane(AdminPane $pane, ?string $group = null): void{
+        //Throw pane into a group if one was provided
+        if ($group !== null){
+            if (!isset($this->groups[$group])){
+                $this->groups[$group] = [];
+            }
+            $this->groups[$group][] = $pane;
+        } else {
+            $this->panes[] = $pane;
+        }
+    }
+
+    public function render(): void{
+        $page = new Page($this->tabula, 'modules/admin/admin.html');
+        $this->tabula->renderer->addScript('admin/admin.js');
+
+        $groups = $this->prepareGroups();
+        $activeItem = $this->activeItem();
+        
+        $errors = $this->tabula->session->getErrors();
+        $semantic = $this->tabula->registry->getUriBase().'/vendor/semantic/ui/dist/';
+        $semanticJs = $semantic . 'semantic.min.js';
+        $semanticCss = $semantic . 'semantic.min.css';
+
+        $page->set('adminUrl',$this->tabula->registry->getUriBase().'admin?pane=');
+        $page->set('groups',$groups);
+        $page->set('items',$this->panes);
+        $page->set('activeItem',$activeItem);
+
+        $page->set('errors',$errors);
+        $page->set('semanticJs',$semanticJs);
+        $page->set('semanticCss',$semanticCss);
+
+        $page->render();
+    }
+
+    /**
+     * Prepare groups for render
+     */
+    private function prepareGroups(): array{
+        $groups = [];
+
+        foreach ($this->groups as $groupname => $group) {
+            $groups[] = [
+                "name" => $groupname,
+                "active" => $this->groupActive($group),
+                "items" => $group
+            ];
+        }
+
+        return $groups;
+    }
+
+    //Check if any pane in a group is active, to expand the group
+    private function groupActive(array $group): bool{
+        $currentPane = $this->tabula->registry->getRequest()->get("pane");
+        if ($currentPane === null) return false;
+        foreach ($group as $pane){
+            if ($currentPane === $pane->getSlug()){
+                $this->activePane = $pane;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Return active pane, if there is one
+     */
+    private function activeItem(){
+        $currentPane = $this->tabula->registry->getRequest()->get("pane");
+        if ($this->activePane instanceof AdminPane){
+            return $this->activePane;
+        }
+        foreach ($this->panes as $pane){
+            if ($currentPane === $pane->getSlug()) return $pane;
+        }
+    }
+}
---
+++ src/Admin/AdminPane.php
@@ -0,0 +1,40 @@
+<?php 
+namespace Tabula\Admin;
+
+abstract class AdminPane {
+    public $tabula;
+
+    public function __construct($tabula){
+        $this->tabula = $tabula;
+    }
+
+    /**
+     * Return the body HTML of your pane here
+     * Will be thrown into a raw tag, so please escape
+     * your shit with the twig renderer first <3
+     */
+    abstract public function render(): string;
+
+    /**
+     * Return the name of your admin pane,
+     * for the menu
+     */
+    abstract public function getName(): string;
+
+    /**
+     * Return a url-friendly slug for your pane
+     */
+    abstract public function getSlug(): string;
+
+    /**
+     * Return an icon for the menu if you want to
+     */
+    abstract public function getIcon(): ?string;
+
+    /**
+     * Check if this pane is active
+     */
+    public function isActive(): bool{
+        return $this->getSlug() === $this->tabula->registry->getRequest()->get("pane");
+    }
+}
